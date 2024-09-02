2316541a7916fb21f65a077fe64f43cf994f41e4
Sam Davis Martin <130231103+samdavis007@users.noreply.github.com>
2024-09-02 22:08:30 +0530

    Add files via upload

---
+++ sky hooper game 2d/Assets/Scripts/GameManager.cs
@@ -0,0 +1,76 @@
+using UnityEngine;
+using UnityEngine.UI;
+
+[DefaultExecutionOrder(-1)]
+public class GameManager : MonoBehaviour
+{
+    public static GameManager Instance { get; private set; }
+
+    [SerializeField] private Player player;
+    [SerializeField] private Spawner spawner;
+    [SerializeField] private Text scoreText;
+    [SerializeField] private GameObject playButton;
+    [SerializeField] private GameObject gameOver;
+
+    public int score { get; private set; } = 0;
+
+    private void Awake()
+    {
+        if (Instance != null) {
+            DestroyImmediate(gameObject);
+        } else {
+            Instance = this;
+        }
+    }
+
+    private void OnDestroy()
+    {
+        if (Instance == this) {
+            Instance = null;
+        }
+    }
+
+    private void Start()
+    {
+        Pause();
+    }
+
+    public void Pause()
+    {
+        Time.timeScale = 0f;
+        player.enabled = false;
+    }
+
+    public void Play()
+    {
+        score = 0;
+        scoreText.text = score.ToString();
+
+        playButton.SetActive(false);
+        gameOver.SetActive(false);
+
+        Time.timeScale = 1f;
+        player.enabled = true;
+
+        Pipes[] pipes = FindObjectsOfType<Pipes>();
+
+        for (int i = 0; i < pipes.Length; i++) {
+            Destroy(pipes[i].gameObject);
+        }
+    }
+
+    public void GameOver()
+    {
+        playButton.SetActive(true);
+        gameOver.SetActive(true);
+
+        Pause();
+    }
+
+    public void IncreaseScore()
+    {
+        score++;
+        scoreText.text = score.ToString();
+    }
+
+}
