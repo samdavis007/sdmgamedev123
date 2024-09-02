baf661013f8083845ad657dc792eedcb9e50dca2
Sam Davis Martin <130231103+samdavis007@users.noreply.github.com>
2024-09-02 22:14:31 +0530

    Add files via upload

---
+++ slice master game 2d/Assets/Scripts/Blade.cs
@@ -0,0 +1,74 @@
+using UnityEngine;
+
+public class Blade : MonoBehaviour
+{
+    public float sliceForce = 5f;
+    public float minSliceVelocity = 0.01f;
+
+    private Camera mainCamera;
+    private Collider sliceCollider;
+    private TrailRenderer sliceTrail;
+
+    public Vector3 direction { get; private set; }
+    public bool slicing { get; private set; }
+
+    private void Awake()
+    {
+        mainCamera = Camera.main;
+        sliceCollider = GetComponent<Collider>();
+        sliceTrail = GetComponentInChildren<TrailRenderer>();
+    }
+
+    private void OnEnable()
+    {
+        StopSlice();
+    }
+
+    private void OnDisable()
+    {
+        StopSlice();
+    }
+
+    private void Update()
+    {
+        if (Input.GetMouseButtonDown(0)) {
+            StartSlice();
+        } else if (Input.GetMouseButtonUp(0)) {
+            StopSlice();
+        } else if (slicing) {
+            ContinueSlice();
+        }
+    }
+
+    private void StartSlice()
+    {
+        Vector3 position = mainCamera.ScreenToWorldPoint(Input.mousePosition);
+        position.z = 0f;
+        transform.position = position;
+
+        slicing = true;
+        sliceCollider.enabled = true;
+        sliceTrail.enabled = true;
+        sliceTrail.Clear();
+    }
+
+    private void StopSlice()
+    {
+        slicing = false;
+        sliceCollider.enabled = false;
+        sliceTrail.enabled = false;
+    }
+
+    private void ContinueSlice()
+    {
+        Vector3 newPosition = mainCamera.ScreenToWorldPoint(Input.mousePosition);
+        newPosition.z = 0f;
+        direction = newPosition - transform.position;
+
+        float velocity = direction.magnitude / Time.deltaTime;
+        sliceCollider.enabled = velocity > minSliceVelocity;
+
+        transform.position = newPosition;
+    }
+
+}
---
+++ slice master game 2d/Assets/Scripts/GameManager.cs
@@ -0,0 +1,123 @@
+using System.Collections;
+using UnityEngine;
+using UnityEngine.UI;
+
+[DefaultExecutionOrder(-1)]
+public class GameManager : MonoBehaviour
+{
+    public static GameManager Instance { get; private set; }
+
+    [SerializeField] private Blade blade;
+    [SerializeField] private Spawner spawner;
+    [SerializeField] private Text scoreText;
+    [SerializeField] private Image fadeImage;
+
+    public int score { get; private set; } = 0;
+
+    private void Awake()
+    {
+        if (Instance != null) {
+            DestroyImmediate(gameObject);
+        } else {
+            Instance = this;
+        }
+    }
+
+    private void OnDestroy()
+    {
+        if (Instance == this) {
+            Instance = null;
+        }
+    }
+
+    private void Start()
+    {
+        NewGame();
+    }
+
+    private void NewGame()
+    {
+        Time.timeScale = 1f;
+
+        ClearScene();
+
+        blade.enabled = true;
+        spawner.enabled = true;
+
+        score = 0;
+        scoreText.text = score.ToString();
+    }
+
+    private void ClearScene()
+    {
+        Fruit[] fruits = FindObjectsOfType<Fruit>();
+
+        foreach (Fruit fruit in fruits) {
+            Destroy(fruit.gameObject);
+        }
+
+        Bomb[] bombs = FindObjectsOfType<Bomb>();
+
+        foreach (Bomb bomb in bombs) {
+            Destroy(bomb.gameObject);
+        }
+    }
+
+    public void IncreaseScore(int points)
+    {
+        score += points;
+        scoreText.text = score.ToString();
+
+        float hiscore = PlayerPrefs.GetFloat("hiscore", 0);
+
+        if (score > hiscore)
+        {
+            hiscore = score;
+            PlayerPrefs.SetFloat("hiscore", hiscore);
+        }
+    }
+
+    public void Explode()
+    {
+        blade.enabled = false;
+        spawner.enabled = false;
+
+        StartCoroutine(ExplodeSequence());
+    }
+
+    private IEnumerator ExplodeSequence()
+    {
+        float elapsed = 0f;
+        float duration = 0.5f;
+
+        // Fade to white
+        while (elapsed < duration)
+        {
+            float t = Mathf.Clamp01(elapsed / duration);
+            fadeImage.color = Color.Lerp(Color.clear, Color.white, t);
+
+            Time.timeScale = 1f - t;
+            elapsed += Time.unscaledDeltaTime;
+
+            yield return null;
+        }
+
+        yield return new WaitForSecondsRealtime(1f);
+
+        NewGame();
+
+        elapsed = 0f;
+
+        // Fade back in
+        while (elapsed < duration)
+        {
+            float t = Mathf.Clamp01(elapsed / duration);
+            fadeImage.color = Color.Lerp(Color.white, Color.clear, t);
+
+            elapsed += Time.unscaledDeltaTime;
+
+            yield return null;
+        }
+    }
+
+}
